1a7908d9a7279e2ed8d7e6460246a69d302bfcd5
Chris <stac1221@gmail.com>
2020-06-17 12:22:22 -0500

    Created child component for contents of 'Header' to be repeated for separate sticky header element

---
+++ src/components/HeaderContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container flex jcb w-100">
+    <div class="left">
+      <a class="link-1 goto-top">
+        <h1 class="page-title">Chris Stack</h1>
+        <svg class="icon icon-arrow-up"><use xlink:href="#icon-arrow-up"></use></svg>
+      </a>
+    </div>
+    <div class="right">
+      <nav>
+        <ul class="flex">
+          <li>
+            <a class="link-1 goto-projects">Projects</a>
+          </li>
+          <li>
+            <a class="link-1 goto-resume">Résumé</a>
+          </li>
+          <li>
+            <a class="link-1 goto-contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderContent'
+}
+</script>
--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -1,18 +1,21 @@
 <template>
-  <header class="header flex aic">
-    <div class="container flex jcb w-100">
-      <div class="left">
-        <h1>Chris Stack</h1>
-      </div>
-      <div class="right">
-        <nav>
-          <ul class="flex">
-            <li><a class="link-1" href="#">Projects</a></li>
-            <li><a class="link-1" href="#">Résumé</a></li>
-            <li><a class="link-1" href="#">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
+  <div>
+    <header class="header">
+      <HeaderContent />
+    </header>
+    <header class="sticky-header">
+      <HeaderContent />
+    </header>
+  </div>
 </template>
+
+<script>
+import HeaderContent from '../HeaderContent'
+
+export default {
+  name: 'Header',
+  components: {
+    HeaderContent
+  }
+}
+</script>
