2753751070b2cfc9d5dd2bf84e5b08525a356c5e
Chris <stac1221@gmail.com>
2020-06-18 01:39:27 -0500

    Broke header nav out into own component to be used in multiple places

--- src/components/sections/header/HeaderContent.vue
+++ src/components/sections/header/HeaderContent.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class="container flex jcb w-100">
-    <div class="left">
-      <a class="link-1 goto-top">
-        <h1 class="page-title">Chris Stack</h1>
-        <svg class="icon icon-arrow-up"><use xlink:href="#icon-arrow-up"></use></svg>
-      </a>
-    </div>
-    <div class="right">
-      <nav>
-        <ul class="flex">
-          <li>
-            <a class="link-1 goto-projects">Projects</a>
-          </li>
-          <li>
-            <a class="link-1 goto-resume">Résumé</a>
-          </li>
-          <li>
-            <a class="link-1 goto-contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="container flex jcb aic w-100">
+    <a class="link-1 goto-top">
+      <h1 class="page-title">Chris Stack</h1>
+      <svg class="icon icon-arrow-up"><use xlink:href="#icon-arrow-up"></use></svg>
+    </a>
+    <HeaderNav />
+    <a class="navicon"></a>
   </div>
 </template>
 
 <script>
+import HeaderNav from './HeaderNav'
+
 export default {
-  name: 'HeaderContent'
+  name: 'HeaderContent',
+  components: {
+    HeaderNav,
+  }
 }
 </script>
---
+++ src/components/sections/header/HeaderNav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <ul class="flex">
+      <li>
+        <a :class="`link-1 goto-projects${compCustomClass}`">Projects</a>
+      </li>
+      <li>
+        <a :class="`link-1 goto-resume${compCustomClass}`">Résumé</a>
+      </li>
+      <li>
+        <a :class="`link-1 goto-contact${compCustomClass}`">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'HeaderNav',
+  props: {
+    customClass: String
+  },
+  computed: {
+    compCustomClass() {
+      let val
+      this.customClass ? val = ` ${this.customClass}` : val = ''
+      return val
+    }
+  }
+}
+</script>
